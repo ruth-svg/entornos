1f08dd67ca204c256e6a1e6de18e8d0866b6b887
ruth-svg <rhtuf@LAPTOP-HGE11LLJ.home>
2022-02-08 22:41:39 +0100

    Segunda subida

--- JavaApplication33/src/Persona/Persona.java
+++ JavaApplication33/src/Persona/Persona.java
@@ -6,9 +6,46 @@
 package Persona;
 
 /**
- *
+ * @since 08-02-2022
  * @author rhtuf
  */
 public class Persona {
     
+    String nombre;
+    String apellidos;
+    String contacto;
+
+    public Persona(String nombre, String apellidos, String contacto) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.contacto = contacto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+    
+    
+    
 }
+
