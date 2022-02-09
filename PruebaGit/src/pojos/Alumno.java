d36d4ad223863ec9bc91945de77d5f91d0f9e1aa
Ruth <ruth@lab03-31.gregoriofer.local>
2022-02-09 09:32:17 +0100

    Otra subida más

--- PruebaGit/src/pojos/Alumno.java
+++ PruebaGit/src/pojos/Alumno.java
@@ -10,45 +10,7 @@ package pojos;
  */
 public class Alumno {
     
-    String q;
-    String f;
-    String i;
-
-    public Alumno(String q, String f, String c) {
-        this.q = q;
-        this.f = f;
-        this.c = c;
-    }
-
-    public String getQ() {
-        return q;
-    }
-
-    public void setQ(String q) {
-        this.q = q;
-    }
-
-    public String getF() {
-        return f;
-    }
-
-    public void setF(String f) {
-        this.f = f;
-    }
-
-    public String getC() {
-        return c;
-    }
-
-    public void setC(String c) {
-        this.c = c;
-    }
-    
-    
-    
-    
-    
-    
+   
 
     
     
